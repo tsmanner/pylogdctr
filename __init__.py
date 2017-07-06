e061c96933e21169e5643c1f3d4d4424f833186e
tsmanner <zephrincochrane@gmail.com>
2017-07-06 13:53:13 -0400

    Changed to use str.format instead of f-strings.
    Compatible with more versions of Python, especially 3.0-3.5.
    Added docstrings.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,18 @@
+"""
+Defines the `logs` decorator.
+
+`@logs` adds a Logger to each decorated class by adding a new member variable `self._log`.
+The logger is automatically named by module + classname. i.e. `mypackage.mymodule.MyClass`.
+
+Usage (mymodule.py):
+
+@logs
+class MyClass:
+    def some_function(self):
+        self._log.debug("Debug Message.")  # self._log.name is "mymodule.MyClass"
+"""
 import logging
 
 def logs(cls):
-    cls._log = logging.getLogger(f"{cls.__module__}.{cls.__name__}")
+    cls._log = logging.getLogger("{modulename}.{cls.classname}".format(modulename=cls.__module__, classname=cls.__name__)
     return cls
